552eefa4c2e5fad61dbf26bfbbe3e6658735290d
youngrok <yr951103@gmail.com>
2024-09-15 17:26:40 +0900

    기계 중심의 시간
    - Instant
    - Epoch time: UTC(1970-01-01 00:00:00)부터 현재까지 경과된 시간을 초 단위로 표현

---
+++ src/time/InstantMain.java
@@ -0,0 +1,31 @@
+package time;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
+public class InstantMain {
+
+    public static void main(String[] args) {
+        // 생성
+        Instant now = Instant.now(); // UTC 기준
+        System.out.println("now = " + now);
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        Instant from = Instant.from(zonedDateTime);
+        System.out.println("from = " + from);
+
+        Instant epochStart = Instant.ofEpochSecond(0);
+//        Instant epochStart = Instant.ofEpochSecond(120);
+        System.out.println("epochSecond = " + epochStart);
+
+        // 계산
+        Instant later = epochStart.plusSeconds(3600);
+        System.out.println("later = " + later);
+
+        // 조회
+        long laterEpochSecond = later.getEpochSecond();
+        System.out.println("laterEpochSecond = " + laterEpochSecond);
+
+
+    }
+}
