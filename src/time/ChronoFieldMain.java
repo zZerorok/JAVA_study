ca459593ac1197937c8994863790b1f40e608649
youngrok <yr951103@gmail.com>
2024-09-18 11:27:39 +0900

    날짜와 시간 인터페이스
    - ChronoField

---
+++ src/time/ChronoFieldMain.java
@@ -0,0 +1,17 @@
+package time;
+
+import java.time.temporal.ChronoField;
+
+public class ChronoFieldMain {
+
+    public static void main(String[] args) {
+        ChronoField[] values = ChronoField.values();
+        for (ChronoField value : values) {
+//            System.out.println("value = " + value);
+            System.out.println(value + ", range = " + value.range());
+        }
+
+        System.out.println("MONTH_OF_YEAR = " + ChronoField.MONTH_OF_YEAR.range());
+        System.out.println("DAY_OF_MONTH = " + ChronoField.DAY_OF_MONTH.range());
+    }
+}
