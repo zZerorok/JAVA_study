5c61486eaa608b4823006a54a1c2681fd2e58bf8
youngrok <yr951103@gmail.com>
2024-09-30 16:33:36 +0900

    익명 클래스 활용 예제2 주석 변경

--- src/nested/anonymous/ex/Ex1RefMainV2.java
+++ src/nested/anonymous/ex/Ex1RefMainV2.java
@@ -6,9 +6,7 @@ public class Ex1RefMainV2 {
 
     public static void hello(Process process) {
         System.out.println("프로그램 시작");
-        // 코드 조각 시작
         process.run();
-        // 코드 조각 종료
         System.out.println("프로그램 종료");
     }
 
@@ -18,6 +16,7 @@ public class Ex1RefMainV2 {
 
             @Override
             public void run() {
+                // 코드 조각
                 int randomValue = new Random().nextInt(6) + 1;
                 System.out.println("주사위 = " + randomValue);
             }
@@ -27,6 +26,7 @@ public class Ex1RefMainV2 {
 
             @Override
             public void run() {
+                // 코드 조각
                 for (int i = 0; i < 3; i++) {
                     System.out.println("i = " + i);
                 }
--- src/nested/anonymous/ex/Ex1RefMainV4.java
+++ src/nested/anonymous/ex/Ex1RefMainV4.java
@@ -6,9 +6,7 @@ public class Ex1RefMainV4 {
 
     public static void hello(Process process) {
         System.out.println("프로그램 시작");
-        // 코드 조각 시작
         process.run();
-        // 코드 조각 종료
         System.out.println("프로그램 종료");
     }
 
@@ -19,6 +17,7 @@ public class Ex1RefMainV4 {
 
             @Override
             public void run() {
+                // 코드 조각
                 int randomValue = new Random().nextInt(6) + 1;
                 System.out.println("주사위 = " + randomValue);
             }
@@ -28,6 +27,7 @@ public class Ex1RefMainV4 {
 
             @Override
             public void run() {
+                // 코드 조각
                 for (int i = 0; i < 3; i++) {
                     System.out.println("i = " + i);
                 }
