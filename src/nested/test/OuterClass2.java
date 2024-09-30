dba652e85756de0df0f6dfdd98dcbf79fe891a48
youngrok <yr951103@gmail.com>
2024-09-30 16:59:47 +0900

    내부 클래스 예제

---
+++ src/nested/test/OuterClass2.java
@@ -0,0 +1,11 @@
+package nested.test;
+
+public class OuterClass2 {
+
+    class InnerClass {
+
+        public void hello() {
+            System.out.println("InnerClass.hello");
+        }
+    }
+}
