c8a2b7df22cb23db477c0b3918b67878a7619081
youngrok <yr951103@gmail.com>
2024-10-04 13:47:58 +0900

    [예외 처리] 체크 예외 - 예외를 처리하지 않고 던지기

--- src/exception/basic/checked/Service.java
+++ src/exception/basic/checked/Service.java
@@ -16,4 +16,12 @@ public class Service {
         }
         System.out.println("정상 흐름");
     }
+
+    /**
+     * 체크 예외를 밖으로 던지는 코드
+     * 체크 예외는 예외를 잡지 않고 밖으로 던지려면 throws 예외를 메서드에 필수로 선언해야 한다.
+     */
+    public void catchThrow() throws MyCheckedException {
+        client.call();
+    }
 }
