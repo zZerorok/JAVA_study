c943b2b2629812a01f6b382a8554295915ec8526
youngrok <yr951103@gmail.com>
2024-10-06 23:50:26 +0900

    [예외 처리] 예외 처리 도입 - 시작
    - NetworkClientExceptionV2 예외 객체 생성
    - 예외 객체 던지기

---
+++ src/exception/ex2/MainV2.java
@@ -0,0 +1,26 @@
+package exception.ex2;
+
+import java.util.Scanner;
+
+public class MainV2 {
+
+    public static void main(String[] args) throws NetworkClientExceptionV2 {
+        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("전송할 문자: ");
+            String input = scanner.nextLine();
+
+            // 검증 로직
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            // 정상 로직
+            networkService.sendMessage(input);
+            System.out.println();
+        }
+        System.out.println("프로그램을 종료합니다.");
+    }
+}
