69d112338a6bba532b39c79886f5d8edd6eb755a
youngrok <yr951103@gmail.com>
2024-10-09 15:32:45 +0900

    [예외 처리] 예외 처리 도입 - 리소스 반환 문제
    - 다른 예외 발생 시 disconnect()가 호출되지 않는 문제 가정

--- src/exception/ex2/NetworkClientV2.java
+++ src/exception/ex2/NetworkClientV2.java
@@ -21,6 +21,8 @@ public class NetworkClientV2 {
     public void send(String data) throws NetworkClientExceptionV2 {
         if (sendError) {
             throw new NetworkClientExceptionV2("sendError", address + " 서버에 데이터 전송 실패: " + data);
+            // 중간에 다른 예외가 발생했다고 가정
+            // throw new RuntimeException("ex");
         }
 
         // 전송 성공
---
+++ src/exception/ex2/NetworkServiceV2_4.java
@@ -0,0 +1,23 @@
+package exception.ex2;
+
+public class NetworkServiceV2_4 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV2 client = new NetworkClientV2(address);
+
+        client.initError(data);
+
+        try {
+            // 정상 흐름
+            client.connect();
+            client.send(data);
+        } catch (NetworkClientExceptionV2 e) {
+            // 예외 흐름
+            System.out.println("[오류] 코드: " + e.getErrorCode() + ", 메세지: " + e.getMessage());
+        }
+
+        // NetworkClientException이 아닌 다른 예외가 발생해 예외가 밖으로 던져지 무시
+        client.disconnect();
+    }
+}
