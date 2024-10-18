eaef38d22658b99390f97324ae11f1b02a30c809
youngrok <yr951103@gmail.com>
2024-10-18 21:51:55 +0900

    [제네릭] 제네릭 기본 예제2
    - 문제: PairTest
    - 풀이: Pair<T1, T2>

---
+++ src/generic/test/ex2/Pair.java
@@ -0,0 +1,30 @@
+package generic.test.ex2;
+
+public class Pair<T1, T2> {
+    private T1 first;
+    private T2 second;
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    public void setFirst(T1 first) {
+        this.first = first;
+    }
+
+    public void setSecond(T2 second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
