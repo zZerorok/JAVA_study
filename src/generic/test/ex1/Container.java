f682a47e209a437809bffd8dd770c2e6909f5fea
youngrok <yr951103@gmail.com>
2024-10-18 21:51:34 +0900

    [제네릭] 제네릭 기본 예제1
    - 문제: ContainerTest
    - 풀이: Container<T>

---
+++ src/generic/test/ex1/Container.java
@@ -0,0 +1,17 @@
+package generic.test.ex1;
+
+public class Container<T> {
+    private T item;
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public boolean isEmpty() {
+        return item == null;
+    }
+}
