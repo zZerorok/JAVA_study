27fac95bfe351c45426a40fb01d2853a9ac10d29
youngrok <yr951103@gmail.com>
2024-10-21 17:26:57 +0900

    [제네릭] 하한 와일드카드 사용 예제 추가
    - 매개변수 타입에 <? super Animal>을 추가하여 Animal 및 Animal 상위 타입을 받을 수 있도록 구현

---
+++ src/generic/test/ex5/WildcardMain2.java
@@ -0,0 +1,28 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardMain2 {
+
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Animal> animalBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        // Animal 포함 상위 타입 전달 가능
+        writeBox(objectBox);
+        writeBox(animalBox);
+//        writeBox(dogBox); // Animal의 자식이기 때문에 불가능
+//        writeBox(catBox); // Animal의 자식이기 때문에 불가능
+
+        Animal animal = animalBox.get();
+        System.out.println("animal = " + animal);
+    }
+
+    static void writeBox(Box<? super Animal> box) {
+        box.set(new Dog("멍멍이", 100));
+    }
+}
