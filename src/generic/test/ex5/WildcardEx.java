a514e5d3067d962af12285dc66d35f48d79bad1b
youngrok <yr951103@gmail.com>
2024-10-21 16:45:55 +0900

    [제네릭] 와일드카드 사용 전 예제
    - 제네릭 메서드를 사용한 예제

---
+++ src/generic/test/ex5/WildcardEx.java
@@ -0,0 +1,21 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+
+public class WildcardEx {
+
+    static <T> void printGenericV1(Box<T> box) {
+        System.out.println("T = " + box.get());
+    }
+
+    static <T extends Animal> void printGenericV2(Box<T> box) {
+        T t = box.get();
+        System.out.println("이름 = " + t.getName());
+    }
+
+    static <T extends Animal> T printAndReturnGeneric(Box<T> box) {
+        T t = box.get();
+        System.out.println("이름 = " + t.getName());
+        return t;
+    }
+}
---
+++ src/generic/test/ex5/WildcardMain1.java
@@ -0,0 +1,21 @@
+package generic.test.ex5;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardMain1 {
+
+    public static void main(String[] args) {
+        Box<Object> objectBox = new Box<>();
+        Box<Dog> dogBox = new Box<>();
+        Box<Cat> catBox = new Box<>();
+
+        dogBox.set(new Dog("멍멍이", 100));
+
+        WildcardEx.printGenericV1(dogBox);
+
+        WildcardEx.printGenericV2(dogBox);
+
+        Dog dog = WildcardEx.printAndReturnGeneric(dogBox);
+    }
+}
