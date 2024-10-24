bd59f6f2041c445d567576bb31fbe44b540c08e2
youngrok <yr951103@gmail.com>
2024-10-24 17:49:52 +0900

    [제네릭] 타입 이레이저 예제
    - 자바에서 컴파일 시 제네릭 타입 T는 실제 타입 정보가 지워지므로 T의 타입을 알 수 없음

---
+++ src/generic/test/ex5/EraserBox.java
@@ -0,0 +1,13 @@
+package generic.test.ex5;
+
+public class EraserBox<T> {
+
+    public boolean instanceCheck(Object param) {
+//        return param instanceof T; // 오류
+        return false;
+    }
+
+    public void create() {
+//        return new T(); // 오류
+    }
+}
