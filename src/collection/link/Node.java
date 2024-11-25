ea249e5914d6e1db6821b900b4fa54070dc6e42d
youngrok <yr951103@gmail.com>
2024-11-25 18:49:12 +0900

    [컬렉션 프레임워크] LinkedList - 노드와 연결 toString() 구현

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -7,4 +7,30 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+    // IDE 생성 toString()
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        Node x = this;
+        builder.append("[");
+        while (x != null) {
+            builder.append(x.item);
+            if (x.next != null) {
+                builder.append(" -> ");
+            }
+            x = x.next;
+        }
+        builder.append("]");
+
+        return builder.toString();
+    }
 }
