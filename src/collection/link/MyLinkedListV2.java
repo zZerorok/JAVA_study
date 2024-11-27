984da82ceaacf33fbfe969fa2edee5db4a26da78
youngrok <yr951103@gmail.com>
2024-11-27 17:02:18 +0900

    [컬렉션 프레임워크] 직접 구현하는 연결 리스트 - 추가와 삭제

---
+++ src/collection/link/MyLinkedListV2.java
@@ -0,0 +1,110 @@
+package collection.link;
+
+public class MyLinkedListV2 {
+    private Node first;
+    private int size = 0;
+
+    public void add(Object element) {
+        Node newNode = new Node(element);
+
+        if (first == null) {
+            first = newNode;
+        } else {
+            Node lastNode = getLastNode();
+            lastNode.next = newNode;
+        }
+
+        size++;
+    }
+
+    private Node getLastNode() {
+        Node x = first;
+
+        while (x.next != null) {
+            x = x.next;
+        }
+
+        return x;
+    }
+
+    // 추가 코드
+    public void add(int index, Object element) {
+        Node newNode = new Node(element);
+
+        if (index == 0) {
+            newNode.next = first;
+            first = newNode;
+        } else {
+            Node prev = getNode(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+
+        size++;
+    }
+
+    public Object set(int index, Object element) {
+        Node x = getNode(index);
+        Object oldValue = x.item;
+        x.item = element;
+        return oldValue;
+    }
+
+    // 추가 코드
+    public Object remove(int index) {
+        Node removeNode = getNode(index);
+        Object removedItem = removeNode.item;
+
+        if (index == 0) {
+            first = removeNode.next;
+        } else {
+            Node prev = getNode(index - 1);
+            prev.next = removeNode.next;
+        }
+
+        removeNode.item = null;
+        removeNode.next = null;
+        size--;
+        return removedItem;
+    }
+
+    public Object get(int index) {
+        Node node = getNode(index);
+        return node.item;
+    }
+
+    private Node getNode(int index) {
+        Node x = first;
+
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+
+        return x;
+    }
+
+    public int indexOf(Object element) {
+        int index = 0;
+
+        for (Node x = first; x != null; x = x.next) {
+            if (element.equals(x.item)) {
+                return index;
+            }
+            index++;
+        }
+
+        return -1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedListV1{" +
+                "first=" + first +
+                ", size=" + size +
+                '}';
+    }
+}
