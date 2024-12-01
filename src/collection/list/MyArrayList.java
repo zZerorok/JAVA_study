13d9cfc07721f1be4b32c2802e0c18c26786e3d9
youngrok <yr951103@gmail.com>
2024-12-01 17:32:56 +0900

    [컬렉션 프레임워크] List 추상화 - 인터페이스 도입
    - 인터페이스 추가: MyList
    - 구현체 추가: MyArrayList(배열 기반 동적 리스트), MyLinkedList(연결 리스트 기반 리스트)

---
+++ src/collection/list/MyArrayList.java
@@ -0,0 +1,98 @@
+package collection.list;
+
+import java.util.Arrays;
+
+public class MyArrayList<E> implements MyList<E> {
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+
+    @Override
+    public void add(int index, E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        shiftRightFrom(index);
+        elementData[index] = e;
+        size++;
+    }
+
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i - 1];
+        }
+    }
+
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        return (E) elementData[index];
+    }
+
+    @Override
+    public E set(int index, E element) {
+        E oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    @Override
+    public E remove(int index) {
+        E oldValue = get(index);
+        shiftLeftFrom(index);
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+    }
+
+    @Override
+    public int indexOf(E e) {
+        for (int index = 0; index < size; index++) {
+            if (e.equals(elementData[index])) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size))
+                + " size = " + size + ", capacity = " + elementData.length;
+    }
+}
---
+++ src/collection/list/MyLinkedList.java
@@ -0,0 +1,141 @@
+package collection.list;
+
+public class MyLinkedList<E> implements MyList<E> {
+    private Node<E> first;
+    private int size = 0;
+
+    @Override
+    public void add(E element) {
+        Node<E> newNode = new Node<>(element);
+
+        if (first == null) {
+            first = newNode;
+        } else {
+            Node<E> lastNode = getLastNode();
+            lastNode.next = newNode;
+        }
+
+        size++;
+    }
+
+    private Node<E> getLastNode() {
+        Node<E> x = first;
+
+        while (x.next != null) {
+            x = x.next;
+        }
+
+        return x;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        Node<E> newNode = new Node<>(element);
+
+        if (index == 0) {
+            newNode.next = first;
+            first = newNode;
+        } else {
+            Node<E> prev = getNode(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+
+        size++;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        Node<E> x = getNode(index);
+        E oldValue = x.item;
+        x.item = element;
+        return oldValue;
+    }
+
+    @Override
+    public E remove(int index) {
+        Node<E> removeNode = getNode(index);
+        E removedItem = removeNode.item;
+
+        if (index == 0) {
+            first = removeNode.next;
+        } else {
+            Node<E> prev = getNode(index - 1);
+            prev.next = removeNode.next;
+        }
+
+        removeNode.item = null;
+        removeNode.next = null;
+        size--;
+        return removedItem;
+    }
+
+    @Override
+    public E get(int index) {
+        Node<E> node = getNode(index);
+        return node.item;
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> x = first;
+
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+
+        return x;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        int index = 0;
+
+        for (Node<E> x = first; x != null; x = x.next) {
+            if (element.equals(x.item)) {
+                return index;
+            }
+            index++;
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedListV1{" +
+                "first=" + first +
+                ", size=" + size +
+                '}';
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+
+        public Node(E item) {
+            this.item = item;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            Node<E> x = this;
+            builder.append("[");
+            while (x != null) {
+                builder.append(x.item);
+                if (x.next != null) {
+                    builder.append(" -> ");
+                }
+                x = x.next;
+            }
+            builder.append("]");
+
+            return builder.toString();
+        }
+    }
+}
