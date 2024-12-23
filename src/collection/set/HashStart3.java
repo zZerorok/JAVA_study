8c5880e96a6960ebbf36fa8813f027be9666034d
youngrok <yr951103@gmail.com>
2024-12-23 13:35:06 +0900

    [컬렉션 프레임워크] Hash - 메모리 낭비

---
+++ src/collection/set/HashStart3.java
@@ -0,0 +1,21 @@
+package collection.set;
+
+import java.util.Arrays;
+
+public class HashStart3 {
+
+    public static void main(String[] args) {
+        Integer[] inputArray = new Integer[100];
+        inputArray[1] = 1;
+        inputArray[2] = 2;
+        inputArray[5] = 5;
+        inputArray[8] = 8;
+        inputArray[14] = 14;
+        inputArray[99] = 99;
+        System.out.println("inputArray = " + Arrays.toString(inputArray));
+
+        int searchValue = 99;
+        Integer result = inputArray[searchValue];
+        System.out.println(result);
+    }
+}
