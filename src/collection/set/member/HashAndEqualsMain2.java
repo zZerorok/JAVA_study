0edaf0c78b7e58166a3b9d0bc6444e102e827867
youngrok <yr951103@gmail.com>
2024-12-24 23:58:00 +0900

    [컬렉션 프레임워크] HashSet - hashCode만 구현한 경우의 예제

---
+++ src/collection/set/member/HashAndEqualsMain2.java
@@ -0,0 +1,27 @@
+package collection.set.member;
+
+import collection.set.MyHashSetV2;
+
+public class HashAndEqualsMain2 {
+
+    public static void main(String[] args) {
+        // 중복 등록
+        MyHashSetV2 set = new MyHashSetV2(10);
+        MemberOnlyHash member1 = new MemberOnlyHash("A");
+        MemberOnlyHash member2 = new MemberOnlyHash("A");
+
+        System.out.println("member1.hashCode() = " + member1.hashCode());
+        System.out.println("member2.hashCode() = " + member2.hashCode());
+        System.out.println("member1.equals(member2) = " + member1.equals(member2));
+
+        set.add(member1);
+        set.add(member2);
+        System.out.println(set);
+
+        // 검색 실패
+        MemberOnlyHash searchValue = new MemberOnlyHash("A");
+        System.out.println("searchValue.hashCode() = " + searchValue.hashCode());
+        boolean contains = set.contains(searchValue);
+        System.out.println("contains = " + contains);
+    }
+}
