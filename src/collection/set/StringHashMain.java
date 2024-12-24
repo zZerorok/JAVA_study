0ca67cd0003852060022d7a9372fd1985774a24b
youngrok <yr951103@gmail.com>
2024-12-24 23:50:39 +0900

    [컬렉션 프레임워크] HashSet - 문자열 해시 코드

---
+++ src/collection/set/StringHashMain.java
@@ -0,0 +1,36 @@
+package collection.set;
+
+public class StringHashMain {
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+        // char
+        char charA = 'A';
+        char charB = 'B';
+        System.out.println("charA = " + (int) charA);
+        System.out.println("charB = " + (int) charB);
+
+        // hashCode
+        System.out.println("hashCode(A) = " + hashCode("A"));
+        System.out.println("hashCode(B) = " + hashCode("B"));
+        System.out.println("hashCode(AB) = " + hashCode("AB"));
+
+        // hashIndex
+        System.out.println("hashIndex(A) = " + hashIndex(hashCode("A")));
+        System.out.println("hashIndex(B) = " + hashIndex(hashCode("B")));
+        System.out.println("hashIndex(AB) = " + hashIndex(hashCode("AB")));
+    }
+
+    static int hashCode(String string) {
+        char[] charArray = string.toCharArray();
+        int sum = 0;
+        for (char c : charArray) {
+            sum = sum + c;
+        }
+        return sum;
+    }
+
+    static int hashIndex(int value) {
+        return value % CAPACITY;
+    }
+}
