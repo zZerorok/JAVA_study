85ae673e72ca07a8307f062e26f5b5b13da43ed6
youngrok <yr951103@gmail.com>
2025-01-10 23:52:05 +0900

    [컬렉션 프레임워크] Set - 자바 Set 예제(Equals, HashCode)

---
+++ src/collection/set/test/Rectangle.java
@@ -0,0 +1,34 @@
+package collection.set.test;
+
+import java.util.Objects;
+
+public class Rectangle {
+    private final int width;
+    private final int height;
+
+    public Rectangle(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return width == rectangle.width && height == rectangle.height;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, height);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
