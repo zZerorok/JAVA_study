a29a941119f343a8f5ee1904b66a1ed70f41ef66
youngrok <yr951103@gmail.com>
2025-01-20 16:23:22 +0900

    [컬렉션 프레임워크] Map 문제와 풀이 - 같은 단어의 수

---
+++ src/collection/map/test/WordFrequencyTest1.java
@@ -0,0 +1,24 @@
+package collection.map.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordFrequencyTest1 {
+
+    public static void main(String[] args) {
+        String text = "orange banana apple apple banana apple";
+
+        // 풀이
+        Map<String, Integer> map = new HashMap<>();
+        String[] words = text.split(" ");
+        for (String word : words) {
+            Integer count = map.get(word);
+            if (count == null) {
+                count = 0;
+            }
+            count++;
+            map.put(word, count);
+        }
+        System.out.println(map);
+    }
+}
